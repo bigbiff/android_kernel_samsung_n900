cc051632dc6d613ad0b43543e5eef8ecb5e4fc0d
bigbiff bigbiff <bigbiff@teamw.in>
2013-10-31 10:09:03 -0400

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Sat Oct 26 08:54:31 EDT 2013"
+#define LINUX_COMPILE_BY "bigbiff"
+#define LINUX_COMPILE_HOST "nas"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
